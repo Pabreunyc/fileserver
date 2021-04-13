e6e78b391e921a8ae0f408cdbcb386be41ec23d6
paul <paul>
2021-04-13 15:27:23 -0400

    04.13.2021: cp_test2.js

--- cp_test2.js
+++ cp_test2.js
@@ -3,14 +3,21 @@ const fs = require('fs'),
     path = require('path'),
     url = require('url');
 
-let srcDirsJSON = require('./public/data/tbl_cbg_web_server_info.json');
+let srcDirsJSON = require('./config/drive_paths_mock.json');
 let srcDir = srcDirsJSON.filter( e => e.is_active == 1)[0];
-console.log('j:', srcDir);
+console.log('srcDir', srcDir);
+;
+if(process.env.SRC_DIR) {
+    console.log('>>>>', process.env.SRC_DIR);
+    srcDir.server_file_path = process.env.SRC_DIR.trim();
+}
 // SVGPathSegCurvetoQuadraticSmoothRel
 
 
 try {
-    console.log(srcDir.server_file_path, fs.realpathSync(srcDir.server_file_path) );
+    console.log(srcDir.server_file_path, fs.existsSync(srcDir.server_file_path) );
+    let url1 = url.pathToFileURL(srcDir.server_file_path);
+    console.log(url1, fs.existsSync(url1) );
 } catch(e) {
     console.log('ERR', e);
 }
